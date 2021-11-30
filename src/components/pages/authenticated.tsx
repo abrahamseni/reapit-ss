4f43c89825b912a69fd60a168875baa32ca05816
abrahamseni <abraham@softwareseni.com>
2021-11-30 16:12:50 +0700

    fix table data

--- src/components/pages/authenticated.tsx
+++ src/components/pages/authenticated.tsx
@@ -1,22 +1,16 @@
 import React, { useEffect, FC, useState } from "react";
-import {
-  Title,
-  BodyText,
-  Table,
-  TableRow,
-  TableHeadersRow,
-  TableHeader,
-  TableCell,
-  Icon,
-} from "@reapit/elements";
+import { Title, BodyText, Table, Pagination, RowProps } from "@reapit/elements";
 import {
   useReapitConnect,
   ReapitConnectSession,
 } from "@reapit/connect-session";
 import { reapitConnectBrowserSession } from "../../core/connect-session";
 import { configurationAppointmentsApiService } from "../../platform-api/configuration-api";
-import { ListItemModel, Properties } from "@reapit/foundations-ts-definitions";
-import { BASE_HEADERS } from "../../constants/api";
+import {
+  ListItemModel,
+  PropertyModel,
+  PropertyModelPagedResult,
+} from "@reapit/foundations-ts-definitions";
 import { useQuery } from "react-query";
 import axios from "../../axios";
 
@@ -25,13 +19,16 @@ export type AuthenticatedProps = {};
 async function getAllProperties({
   queryKey,
 }: {
-  queryKey: [String, ReapitConnectSession | null];
-}): Promise<Properties> {
-  const res = await axios.get("properties/?marketingMode=selling", {
-    headers: {
-      Authorization: `Bearer ${queryKey[1]?.accessToken}`,
-    },
-  });
+  queryKey: [string, ReapitConnectSession | null, number];
+}): Promise<PropertyModelPagedResult> {
+  const res = await axios.get(
+    `properties/?pageNumber=${queryKey[2]}&marketingMode=selling`,
+    {
+      headers: {
+        Authorization: `Bearer ${queryKey[1]?.accessToken}`,
+      },
+    }
+  );
   return res.data;
 }
 
@@ -40,13 +37,15 @@ export const Authenticated: FC<AuthenticatedProps> = () => {
   const [appointmentConfigTypes, setAppointmentConfigTypes] = useState<
     ListItemModel[]
   >([]);
+  const [currentPage, setCurrentPage] = useState<number>(1);
 
   const properties = useQuery<
-    Properties,
+    PropertyModelPagedResult,
     Error,
-    Properties,
-    [String, ReapitConnectSession | null]
-  >(["getAllProperties", connectSession], getAllProperties);
+    PropertyModelPagedResult,
+    [string, ReapitConnectSession | null, number]
+  >(["getAllProperties", connectSession, currentPage], getAllProperties);
+
   const { status, data, error } = properties;
 
   useEffect(() => {
@@ -66,48 +65,48 @@ export const Authenticated: FC<AuthenticatedProps> = () => {
 
   // console.log("Appointment Config Types are: ", appointmentConfigTypes);
 
-  const headers = [
-    "No",
-    "Type",
-    "Address",
-    "Bathrooms",
-    "Bedrooms",
-    "Currency",
-    "Price",
-  ];
+  const [tableData, setTableData] = useState<RowProps[] | undefined>(undefined);
+  useEffect(() => {
+    const expandableContent = {
+      content: (
+        <p>
+          I am more content that is only visible when the 2nd row is expanded
+        </p>
+      ),
+    };
 
-  const renderHeader = (headers: string[]) => {
-    return headers.map((header) => (
-      <TableHeader key={header}>{header}</TableHeader>
-    ));
-  };
-
-  const renderCell = (property, index: number) => {
-    const address = [
-      property.address.line1,
-      property.address.line2,
-      property.address.line3,
-      property.address.line4,
-    ].reduce((acc, el, i) => {
-      if (i === 0) return el;
-      if (el !== "") {
-        return `${acc}, ${el}`;
-      }
-      return acc;
-    }, "");
-
-    return (
-      <>
-        <TableCell>{index + 1}</TableCell>
-        <TableCell>{property.type[0]}</TableCell>
-        <TableCell>{address}</TableCell>
-        <TableCell>{property.bathrooms}</TableCell>
-        <TableCell>{property.bedrooms}</TableCell>
-        <TableCell>{property.currency}</TableCell>
-        <TableCell>{property.selling.price}</TableCell>
-      </>
-    );
-  };
+    let newTableData: RowProps[];
+    if (data && data._embedded) {
+      // @ts-ignore
+      newTableData = data._embedded.map((embed: PropertyModel) => {
+        let cells = [
+          {
+            label: "Type",
+            value: embed.type ?? [0],
+          },
+          { label: "Address", value: embed.address?.buildingName },
+          { label: "Bedrooms", value: embed.bedrooms },
+          {
+            label: "Bathrooms",
+            value: embed.bathrooms,
+          },
+          {
+            label: "Currency",
+            value: embed.currency,
+          },
+          {
+            label: "Price",
+            value: embed.selling?.price,
+          },
+        ];
+        return {
+          cells,
+          expandableContent,
+        };
+      });
+      setTableData(newTableData);
+    }
+  }, [data]);
 
   return (
     <>
@@ -117,17 +116,14 @@ export const Authenticated: FC<AuthenticatedProps> = () => {
       ) : status === "error" ? (
         <BodyText>{error?.message}</BodyText>
       ) : (
-        <Table>
-          <TableHeadersRow>{renderHeader(headers)}</TableHeadersRow>
-          {data &&
-            data["_embedded"].map((property, index: number) => {
-              return (
-                <TableRow key={property.id}>
-                  {renderCell(property, index)}
-                </TableRow>
-              );
-            })}
-        </Table>
+        <Table rows={tableData} />
+      )}
+      {status === "success" && data && (
+        <Pagination
+          callback={setCurrentPage}
+          currentPage={currentPage!}
+          numberPages={data?.pageSize!}
+        />
       )}
     </>
   );
